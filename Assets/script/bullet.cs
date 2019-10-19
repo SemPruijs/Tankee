da93dcc4603cb2cf97385dd65f2383ef0f335c01
SemPruijs <sem@pruijs.nl>
2019-10-19 11:04:12 +0200

    fixed collider bug

--- Assets/script/bullet.cs
+++ Assets/script/bullet.cs
@@ -7,9 +7,11 @@ public class bullet : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Obstacle") {
             Destroy(gameObject);
+        }
+        
+         
+        if (collision.gameObject.tag == "Player") {
+            Destroy(gameObject);
         } 
-        // if (collision.gameObject.tag == "Player") {
-        //     Destroy(gameObject);
-        // } 
     }
 }
