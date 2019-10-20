a10fdca369ee6d01afa7b769c1fae1c051e82072
SemPruijs <sem@pruijs.nl>
2019-10-20 10:59:36 +0200

    add sound track

---
+++ Assets/script/audioManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioClip map1;
+
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource.clip = map1;
+        audioSource.Play();
+    }
+}
