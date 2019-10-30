8c7d0f9370bbf1f4974923803db3daa704d71b4e
SemPruijs <sem@pruijs.nl>
2019-10-30 17:04:58 +0100

    add a count down timer

--- Assets/script/displayManagerInGame.cs
+++ Assets/script/displayManagerInGame.cs
@@ -7,11 +7,30 @@ public class displayManagerInGame : MonoBehaviour
 {
     public Text scorePlayer1;
     public Text scorePlayer2;
+    public Text timeLeftText;
     public baseMovement Player1;
     public baseMovement Player2;
-
+    public float timeLeft = 3; 
+    private string TimeLeftString;
+    public GameObject InGameCanvasGameObject;
     void Update() {
+        //health
         scorePlayer1.text = Player1.heath.ToString();
         scorePlayer2.text = Player2.heath.ToString();
+
+        //count down
+        if (timeLeft <= 0 && timeLeft >= -1 )
+        {
+            TimeLeftString = "Fight!";
+            GameManager.Instance.state = GameManager.State.playing;
+            print("done");
+            InGameCanvasGameObject.SetActive(true);
+        } else if (timeLeft < -1) {
+            TimeLeftString = "";
+        } else {
+            TimeLeftString = timeLeft.ToString("F1");
+        }
+        timeLeftText.text = TimeLeftString;
+        timeLeft -= Time.deltaTime;
     }
 }
