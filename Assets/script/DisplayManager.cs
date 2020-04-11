3ad8347f59e3087ddcbf7b5e96d4ce9e9f4eab7a
SemPruijs <sem@pruijs.nl>
2020-04-11 15:15:19 +0200

    Add pause menu.

--- Assets/script/DisplayManager.cs
+++ Assets/script/DisplayManager.cs
@@ -6,9 +6,11 @@ public class DisplayManager : MonoBehaviour
 {
     public GameObject menu;
     public GameObject inGame;
+    public GameObject pause;
     void Update()
     {
         menu.SetActive(GameManager.Instance.state == GameManager.State.menu);
         inGame.SetActive(GameManager.Instance.state == GameManager.State.playing);
+        pause.SetActive(GameManager.Instance.state == GameManager.State.pause);
     }
 }
